1ef723142d7c5b70c635d3384ec311f3e74c6a34
Oluka Isaac <hilghsilk2damson@gmail.com>
2024-07-20 00:23:22 +0100

    update

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const port = process.env.PORT;
 
 // Example defining a route in Express
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+//   });
 
 
 app.get('/', (req, res) => {
